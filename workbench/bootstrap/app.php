e8c58b88090707d9d1b144824d7dde065c9e866a
Roy Duineveld <royduineveld@gmail.com>
2024-10-01 16:23:58 +0200

    Demo / preview (#4)

---
+++ workbench/bootstrap/app.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+use function Orchestra\Testbench\default_skeleton_path;
+
+return Application::configure(basePath: $APP_BASE_PATH ?? default_skeleton_path())
+    ->withRouting(
+        commands: __DIR__.'/../routes/console.php',
+    )->create();
