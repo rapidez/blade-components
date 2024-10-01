86bfc3c7b50de4099dae61375d07e5557fe083df
Roy Duineveld <royduineveld@gmail.com>
2024-10-01 16:01:40 +0200

    .

--- workbench/routes/console.php
+++ workbench/routes/console.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('components', function () {
+Artisan::command('generate-demo', function () {
     config(['cache.default' => 'array']);
     file_put_contents('demo/components.html', view('rapidez::components-preview'));
 });
