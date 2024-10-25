86a2b1f35cd3cee52d1c00bef22a5e8aee047a15
Roene <roene@justbetter.nl>
2024-10-25 15:33:20 +0200

    Update comment for prose component

---
+++ resources/views/components/prose/prose.blade.php
@@ -0,0 +1,14 @@
+{{--
+This is the default prose, suitable for SEO content and other text.
+The prose classes are not added here directly but are instead located in a separate CSS file: css/components/prose.css.
+When multiple prose components appear on a single page, this setup prevents excessive class clutter in the page source.
+With this solution, only these two classes appear in the source code rather than all individual prose classes.
+Example:
+
+```
+<x-rapidez::prose>Example text</x-rapidez::prose>
+```
+--}}
+<div {{ $attributes->twMerge('prose-custom prose-sm') }}>
+    {{ $slot }}
+</div>
