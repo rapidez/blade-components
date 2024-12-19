5b7b661dc6000186990133db760b30ac07de5937
Jordy de Vulder <jordy@justbetter.nl>
2024-12-19 13:13:35 +0100

    Changed comment within muted button

--- resources/views/components/button/muted.blade.php
+++ resources/views/components/button/muted.blade.php
@@ -1,9 +1,8 @@
 {{--
 The muted button is a variant used for basic use cases.
 Example:
-
 ```
-<x-rapidez::button.conversion>Something</x-rapidez::button.conversion>
+<x-rapidez::button.muted>Something</x-rapidez::button.muted>
 ```
 --}}
 <x-rapidez::button {{ $attributes->twMerge('bg text-muted hover:text hover:bg-emphasis border-b border-b-black/10') }}>
