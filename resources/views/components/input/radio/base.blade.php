9bcac78bc703703bd581dfd27a02415d1c70a895
Roene Verbeek <85165259+Roene-JustBetter@users.noreply.github.com>
2025-01-21 09:33:55 +0100

    Disabled states and updated demo (#18)

--- resources/views/components/input/radio/base.blade.php
+++ resources/views/components/input/radio/base.blade.php
@@ -6,4 +6,4 @@ Examples:
 <x-rapidez::input.radio.base />
 ```
 --}}
-<input type="radio" {{ $attributes->twMerge('cursor-pointer border border-default size-5 text focus:outline-none focus:ring-0 focus:ring-offset-0') }} />
+<input type="radio" {{ $attributes->twMerge('cursor-pointer border border-default size-5 text focus:outline-none focus:ring-0 focus:ring-offset-0 disabled:cursor-not-allowed disabled:bg-muted disabled:border-muted') }} />
