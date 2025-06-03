2d2ded4d32efbaacb93f2062191a5720ad8824a4
Roene <roene@justbetter.nl>
2025-06-03 15:16:34 +0200

    Added fallback for label slot

--- resources/views/components/accordion/accordion.blade.php
+++ resources/views/components/accordion/accordion.blade.php
@@ -5,6 +5,7 @@ Accordion component using `<details>` & `<summary>` HTML elements.
 - `summary` Defines the clickable title that toggles the accordion.
 - `content` The collapsible section that appears when the accordion is opened.
 - `icon`    The icon that is shown, by default this is a chevron that rotates when accordion is opened.
+- `label`   The label that is shown when no summary is provided.
 
 ## Examples
 Basic usage:
@@ -38,14 +39,22 @@ Open one accordion at the time then add a name to the accordion:
     </x-slot:content>
 </x-rapidez::accordion>
 ```
+
+## Fallback
+It is recommended to use the new `summary` slot. For fallback, we have kept the `label` slot so the clickable text is still visible.
+Classes on `<x-slot:label>` won't work anymore.
 --}}
-@slots(['summary', 'content', 'icon'])
+@slots(['summary', 'content', 'icon', 'label'])
 
 <details
     {{ $attributes->twMerge('group/details details-content:h-0 details-content:overflow-clip details-content:transition-[height,content-visibility] details-content:transition-discrete details-content:duration-200 open:details-content:h-auto') }}
 >
     <summary {{ $summary->attributes->twMerge('flex items-center font-medium py-3.5 cursor-pointer list-none') }}>
-        {{ $summary }}
+        @if ($summary->isNotEmpty())
+            {{ $summary }}
+        @elseif ($label)
+            {{ $label }}
+        @endif
 
         @if ($icon)
             @slotdefault('icon')
