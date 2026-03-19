95188ffeb467b55c47f1c10609ef1c5b308f5018
Roene <roene@justbetter.nl>
2026-03-19 13:22:03 +0100

    Fix check border transparent

--- resources/views/components/input/checkbox/base.blade.php
+++ resources/views/components/input/checkbox/base.blade.php
@@ -6,4 +6,4 @@ Examples:
 <x-rapidez::input.checkbox.base />
 ```
 --}}
-<input type="checkbox" {{ $attributes->twMerge('cursor-pointer border border-default rounded-md size-5 text focus:outline-hidden focus:ring-emphasis focus:ring-offset-0 disabled:cursor-not-allowed') }} />
+<input type="checkbox" {{ $attributes->twMerge('cursor-pointer border border-default rounded-md size-5 text checked:border-transparent focus:outline-hidden focus:ring-emphasis focus:ring-offset-0 disabled:cursor-not-allowed') }} />
